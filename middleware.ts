c1b7ffb7610ca785a097e43e169162bca688a09e
Abdullah10992288 <122077258+Abdullah10992288@users.noreply.github.com>
2025-02-19 18:49:40 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: [
-    `${process.env.SITE_URL}/dashboard`,
-    `${process.env.SITE_URL}/dashboard/subscribers`,
-    `${process.env.SITE_URL}/dashboard/menu`,
-  ],
+  matcher: [`/dashboard`, `/dashboard/subscribers`, `/dashboard/menu`],
 };
